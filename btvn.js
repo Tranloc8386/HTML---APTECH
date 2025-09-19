c07a1cfcbfa1a26e2eeabd718019b3d126174d0d
Tranloc06 <tranhuuloc2602@gmail.com>
2025-09-19 23:33:43 +0700

    btvn 18/9

---
+++ btvn.js
@@ -0,0 +1,8 @@
+let a = 2, b = 3;
+console.log("Tong", a+b);
+console.log("Hieu", a-b);
+console.log("Tich", a*b);
+console.log("Chia nguyen", a/b);
+console.log("Chia du", a%b);
+console.log("Luy thua", a**b);
+
---
+++ buoi7/test.js
@@ -0,0 +1,6 @@
+// let a=8;
+// let b=3;
+// console.log(a+b);
+// console.log(--a);
+// console.log(a**3);
+// console.log(34==='8');
