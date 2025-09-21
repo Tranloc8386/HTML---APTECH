836936ea5c44d54d2c14174ebd3d03d3a4324ede
Tranloc06 <tranhuuloc2602@gmail.com>
2025-09-22 00:58:33 +0700

    ok

---
+++ buoi8/tesst.js
@@ -0,0 +1,103 @@
+
+// let a = 10;
+// let b = 32;
+// let tong = (a + b);
+// let kiemtra = (tong != 42);
+
+
+// if (kiemtra) {
+//     console.log('WWin');
+// } else {
+//     console.log('sai');
+// }
+
+
+// let array = [1, 23, 5, 5454, 4];
+// let arrayLength = array.length;
+
+// for (let i = 0; i < arrayLength; i++) {
+//     console.log(array[i]);
+// }
+
+
+// let myInfo = {
+//     name: 'tran loc',
+//     age: '19',
+//     address: 'nghe an'
+// }
+// for (let key in myInfo) {
+//     console.log(myInfo[key]);
+// }
+
+
+// let namejj = 'tran huu loc';
+// for (let key in namejj) {
+//     console.log(namejj[key]);
+// }
+
+
+// let heheh = 'dsdfsdfs';
+// for (let value of heheh) {
+//     console.log(value);
+// }
+
+// let myInfo = {
+//     ten: 'loc',
+//     age: '10'
+// };
+// for (let value of Object.values(myInfo)) {
+//     console.log(myInfo[value]);
+// }
+
+
+// for(let i=1; i<=10; i++)
+// {
+//      console.log(`5 * ${i} = ${5*i}`);
+// }
+
+// 
+//  let sum=0;
+// let i=1;
+// while(i<=10)
+// {
+//     sum= sum+i;
+//     i++;
+// }
+// console.log(sum);
+// let matkhau;
+// do {
+//     matkhau=prompt("  Nhap MK: ");
+// } while (condition);
+
+// function chao(name)
+// {
+//     console.log("chao "+ name);
+// }
+// chao("loc");
+
+
+// function tong(a,b)
+// {
+//     console.log("tong la", + a+b);
+// }
+// tong(23, 88);
+// function mau(dsfsd)
+// {
+//     console.log("xin chao" +dsfsd);
+// }
+
+// mau("lllll");
+
+let helloo = function () {
+    console.log(" hello world");
+}
+helloo();
+
+
+let arrow = (a) => {
+    console.log(" xin chao " + a);
+}
+arrow("Loc");
+
+const tinhTong = (a, b) => { console.log(a + b) };
+tinhTong(2, 8);
