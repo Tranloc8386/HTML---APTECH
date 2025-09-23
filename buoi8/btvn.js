57b06b7222ca698639e66492ec34a286e66519a4
Tranloc06 <tranhuuloc2602@gmail.com>
2025-09-23 11:51:35 +0700

    ok

--- buoi8/btvn.js
+++ buoi8/btvn.js
@@ -1,13 +1,6 @@
-// <!-- Bài 1: Viết một hàm getGrade(score) nhận vào điểm số (0 → 100) và trả về kết quả:
-// "A" nếu điểm ≥ 85
-// "B" nếu điểm từ 70 → 84
-// "C" nếu điểm từ 50 → 69
-// "D" nếu điểm < 50
 
-// Bài 2. Viết một hàm calculateElectricBill(kwh) tính tiền điện theo quy tắc:
-// 0 – 50 kWh: 1.500 đ/kWh
-// 51 – 100 kWh: 2.000 đ/kWh
-// Trên 100 kWh: 3.000 đ/kWh -->
+
+
 
 
 
@@ -19,7 +12,7 @@ function getGrade() {
     } else if (nhapdiem >= 70 && nhapdiem <= 84) {
         console.log("B");
     } else if (nhapdiem >= 50 && nhapdiem <= 69) {
-        console.log("C")
+        console.log("C");
     } else if (nhapdiem >= 0 && nhapdiem < 50) {
         console.log("D");
     } else {
